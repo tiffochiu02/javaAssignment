b143794b318d19d54e9773fdd4c7fb2b1c69f1e4
tiffochiu02 <zhaotiffany2@gmail.com>
2025-03-12 14:58:39 +0000

    Update Column.java

--- cw_db/src/main/java/edu/uob/Column.java
+++ cw_db/src/main/java/edu/uob/Column.java
@@ -9,20 +9,20 @@ public class Column {
     private boolean isPrimaryKey;
 
     public Column(String columnName, String ColumnType) {
-        this.columnName = columnName.toLowerCase();
+        this.columnName = columnName;
         this.columnType = "";
         this.columnIndex = null;
         this.isPrimaryKey = false;
-
     }
 
     public String getColumnName() {return columnName;}
     public void setColumnName(String columnName) {
-        this.columnName = columnName.toLowerCase();
+        this.columnName = columnName;
     }
     public String getColumnType() {return columnType;}
     public Integer getColumnIndex(String columnName) {return columnIndex;}
     public void setColumnIndex(int columnIndex) {this.columnIndex = columnIndex;}
+    //public void setIsPrimaryKey(boolean isPrimaryKey) {this.isPrimaryKey = isPrimaryKey;}
     public boolean isPrimaryKey() {return isPrimaryKey;}
 //    public String toString(Column column) {}
 //    public Column fromString(String column) {}

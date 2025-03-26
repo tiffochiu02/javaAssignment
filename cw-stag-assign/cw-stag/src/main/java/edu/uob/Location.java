812474a2a5f2bbd89c91515455d728500ec7c992
tiffochiu02 <zhaotiffany2@gmail.com>
2025-03-26 15:59:19 +0000

    Update Location.java

--- cw-stag-assign/cw-stag/src/main/java/edu/uob/Location.java
+++ cw-stag-assign/cw-stag/src/main/java/edu/uob/Location.java
@@ -3,7 +3,9 @@ package edu.uob;
 import com.alexmerz.graphviz.objects.Edge;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Objects;
 
 //It is worth noting that every game has a "special" location that is the starting point for each player
 // adventure. The start location can be called anything we like,
@@ -42,12 +44,21 @@ public class Location extends GameEntity{
 
     public void addArtefact(Artefact artefact){
         int maxIndex = 0;
-        for(Map.Entry<Integer,Artefact> entry : this.artefacts.entrySet()){
+        for(Map.Entry<Integer,Artefact> entry : this.getArtefacts().entrySet()){
             if(entry.getKey() > maxIndex){
                 maxIndex = entry.getKey();
             }
         }
         this.artefacts.put(maxIndex+1,artefact);
+    }
+    public void removeArtefact(String artefactName){
+        Iterator<Map.Entry<Integer, Artefact>> iterator = this.getArtefacts().entrySet().iterator();
+        while(iterator.hasNext()){
+            Map.Entry<Integer, Artefact> entry = iterator.next();
+            if(Objects.equals(entry.getValue().getName(), artefactName)){
+                iterator.remove();
+            }
+        }
     }
     public void addFurniture(Furniture newFurniture){
         int maxIndex = 0;
@@ -67,26 +78,38 @@ public class Location extends GameEntity{
         }
         this.characters.put(maxIndex+1,character);
     }
-    public void addPath(int index, String toName){this.paths.put(index,toName);}
+    public void addPath(String toName){
+        int maxIndex = 0;
+        for(Map.Entry<Integer,String> entry : this.getPaths().entrySet()){
+            if(entry.getKey() > maxIndex){
+                maxIndex = entry.getKey();
+            }
+        }
+        int index = maxIndex+1;
+        this.paths.put(index,toName);
+    }
     public HashMap<Integer, String> getPaths(){return this.paths;}
 
     public String listAllArtefacts(){
         StringBuilder names = new StringBuilder();
-        for(int i = 0; i<this.artefacts.size()-1; i++){
-            String name = this.artefacts.get(i).getName();
-            names.append(name).append(", ");
+//for(int i = 0; i<this.artefacts.size()-1; i++){
+        for(Map.Entry<Integer,Artefact> entry : this.getArtefacts().entrySet()){
+            names.append(entry.getValue().getName()).append(", ");
         }
-        names.append(this.artefacts.get(this.artefacts.size()-1).getName());
+        //names.deleteCharAt(names.length()-1);
         return names.toString();
     }
 
     public String listAllFurniture(){
         StringBuilder names = new StringBuilder();
-        for(int i = 0; i<this.furniture.size()-1; i++){
-            String name = this.furniture.get(i).getName();
-            names.append(name).append(", ");
+//        for(int i = 0; i<this.furniture.size()-1; i++){
+//            String name = this.furniture.get(i).getName();
+//            names.append(name).append(", ");
+//        }
+        for(Map.Entry<Integer,Furniture> entry : this.getFurniture().entrySet()){
+            names.append(entry.getValue().getName()).append(", ");
         }
-        names.append(this.furniture.get(this.furniture.size()-1).getName());
+        //names.deleteCharAt(names.length()-1);
         return names.toString();
     }
 }
